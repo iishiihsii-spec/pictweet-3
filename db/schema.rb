7fcaea3cb101bb1adfeac433a0c2a53f250bd036
ko <iishiihsii@gmail.com>
2026-05-11 14:23:45 +0900

    マイページの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_05_11_045151) do
+ActiveRecord::Schema[7.1].define(version: 2026_05_11_052100) do
   create_table "tweets", charset: "utf8mb3", force: :cascade do |t|
-    t.string "name"
     t.string "text"
     t.text "image"
     t.datetime "created_at", null: false
